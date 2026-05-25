e0ff34a205a79e72122f03f6e73f6a28a65cb320
TomasFranchino <tomi.franc2@gmail.com>
2026-05-25 17:10:41 -0300

    axios credentials fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { toast } from 'sonner';
 
+axios.defaults.withCredentials = true;
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
